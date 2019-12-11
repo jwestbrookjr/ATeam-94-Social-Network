bcb3871283a0b409db6c085572f2a76401c6493e
sampeaslee <55960529+sampeaslee@users.noreply.github.com>
2019-12-10 22:43:55 -0600

    Cast text from a file toLowerCase

--- src/application/SocialNetwork.java
+++ src/application/SocialNetwork.java
@@ -4,10 +4,9 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Scanner;
-import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
+
+
 
 /**
  * Class which represents a social network.
@@ -66,76 +65,82 @@ public class SocialNetwork implements SocialNetworkADT {
 		centralUser = cu;
 	}
 
-	/**
-	 * Parses the contents of the input file to create a SocialNetWork. Stores
-	 * users in a graph depending on the contents of the input file.
-	 * 
-	 * @throws InvalidInputFileFormatException - if input file does not valid
-	 *                                         format.
-	 * @throws FileNotFoundException           - if file is not found.
-	 */
-	public void createSocialNetWork(File inputFile)
-			throws InvalidInputFileFormatException, FileNotFoundException {
-		Scanner scn = new Scanner(inputFile);
-		// Read the contents of the input file that will create the initial
-		// social network.
-		while (scn.hasNextLine()) {
-			// Get current line of file.
-			String currentLine = scn.nextLine();
-			// Split into String array containing the words in the line.
-			String[] words = currentLine.split(" ");
-			// Get the first word in the line which is the command.
-			String command = words[0];
-			// Creates the social network.
-			updateSocialNetwork(command, words);
-		}
-		scn.close();
-	}
+	   /**
+     * Parses the contents of the input file to create a SocialNetWork. Stores
+     * users in a graph depending on the contents of the input file
+     * 
+     * @throws InvalidInputFileFormatException - if input file does not valid
+     *                                         format
+     * @throws FileNotFoundException           - if file is not found
+     */
+    public void createSocialNetWork(File inputFile)
+            throws InvalidInputFileFormatException, FileNotFoundException {
+        Scanner scn = new Scanner(inputFile);
+        // Read the contents of the input file that will create the initial
+        // social network.
+        while (scn.hasNextLine()) {
+            // Get current line of file.
+            String currentLine = scn.nextLine().toLowerCase();
+            // Split into String array containing the words in the line.
+            String[] words = currentLine.split(" ");
+            // Get the first word in the line which is the command.
+            String command = words[0];
+            // Creates the social network.
+            updateSocialNetwork(command, words);
+        }
+        scn.close();
+    }
 
-	/**
-	 * Update the contents of the SocialNetwork depending on the command given
-	 * from the file used to initially create the SocialNetwork or when a change
-	 * is made via the GUI.
-	 * 
-	 * @param command - add, remove, or set central user.
-	 * @param words   - array of strings containing the name(s) that come after
-	 *                the command.
-	 * @throws InvalidInputFileFormatException - if input file is not valid
-	 *                                         format.
-	 */
-	public void updateSocialNetwork(String command, String[] cmdLine)
-			throws InvalidInputFileFormatException {
-		if (command.equals("a")) {
-			// Add a user or create a new friendship
-			// between users.
-			if (cmdLine.length == 2) {
-				graph.addVertex(cmdLine[1]);
-			} else if (cmdLine.length == 3) {
-				graph.addEdge(cmdLine[1], cmdLine[2]);
-			} else {
-				throw new InvalidInputFileFormatException();
-			}
-		} else if (command.equals("r")) {
-			// Remove a user and all users' friendships,
-			// or remove a friendship between two users.
-			if (cmdLine.length == 2) {
-				graph.removeVertex(cmdLine[1]);
-			} else if (cmdLine.length == 3) {
-				graph.removeEdge(cmdLine[1], cmdLine[2]);
-			} else {
-				throw new InvalidInputFileFormatException();
-			}
-		} else if (command.equals("s")) {
-			if (cmdLine.length == 2) {
-				// System.out.println(cmdLine[1]);
-				setCentralUser(graph.search(cmdLine[1]));
-			} else {
-				throw new InvalidInputFileFormatException();
-			}
-		} else {
-			throw new InvalidInputFileFormatException();
-		}
-	}
+    /**
+     * Update the contents of the SocialNetwork depending on the command given
+     * from the file used to initially create the SocialNetwork or when a change
+     * is made via the GUI.
+     * 
+     * @param command - add, remove, or set central user.
+     * @param words   - array of strings containing the name(s) that come after
+     *                the command.
+     * @throws InvalidInputFileFormatException - if input file is not valid
+     *                                         format.
+     */
+    public void updateSocialNetwork(String command, String[] cmdLine)
+            throws InvalidInputFileFormatException {
+        if (command.equals("a")) {
+            // Add a user or create a new friendship
+            // between users.
+            if (cmdLine.length == 2) {
+                graph.addVertex(cmdLine[1]);
+            } else if (cmdLine.length == 3) {
+                graph.addEdge(cmdLine[1], cmdLine[2]);
+            } else {
+                throw new InvalidInputFileFormatException();
+            }
+        } else if (command.equals("r")) {
+           
+            // Remove a user and all users' friendships,
+            // or remove a friendship between two users.
+            if (cmdLine.length == 2) {
+                // If the central user set is removed need to update the 
+                // centralUser field
+                if(centralUser != null && centralUser.getUserName().
+                    equals(cmdLine[1])) {
+                    setCentralUser(null);
+                }           
+                graph.removeVertex(cmdLine[1]);
+            } else if (cmdLine.length == 3) {
+                graph.removeEdge(cmdLine[1], cmdLine[2]);
+            } else {
+                throw new InvalidInputFileFormatException();
+            }
+        } else if (command.equals("s")) {
+            if (cmdLine.length == 2) {
+                setCentralUser(graph.search(cmdLine[1]));
+            } else {
+                throw new InvalidInputFileFormatException();
+            }
+        } else {
+            throw new InvalidInputFileFormatException();
+        }
+    }
 
 	/**
 	 * Copies contents of a text file line by line to the log file. Used when a
